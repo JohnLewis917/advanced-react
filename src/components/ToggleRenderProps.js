7458ad0d07da9b2e2c5cf2047623612439555793
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-13 14:18:31 -0700

    done

---
+++ src/components/ToggleRenderProps.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import StyleHOC from './StyleHOC'
+
+
+
+const ToggleRenderProps = (props) => {
+    const [on, setOn] = useState(false)
+    const {style} = props
+    return (
+        <div>
+            {props.render({on, setOn, style})}
+            
+        </div>
+    )
+}
+
+export default StyleHOC(ToggleRenderProps)
