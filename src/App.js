a689431982f2ac75fa137a5b472c48456e5819ce
John Lewis <johnlewis@MacBook-Pro.local>
2019-11-13 14:19:40 -0700

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Counter from './components/Counter'
+import PianoList from './components/PianoList'
+import Poke from './components/Poke'
+import ComplexButton from './components/ComplexButton'
+import SimplerButton from './components/SimplerButton'
+import Toggle from './components/Toggle'
+import ToggleRenderProps from './components/ToggleRenderProps'
+import ToggleRPC from './components/ToggleRPC'
+
 import './App.css';
 
 function App() {
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      {/* <Counter />
+      <PianoList />
+      <Poke /> */}
+      {/* <ComplexButton disabled/>
+      <ComplexButton/>
+      <SimplerButton disabled/>
+      <SimplerButton/> */}
+      <Toggle>
+          <h1>What do you do when you see a spaceman?</h1>
+      </Toggle>
+      <ToggleRenderProps 
+      render = {({on, setOn, style}) => (
+        <div>
+          {on && <h1>Park your car, man</h1>}
+          <button style={style} onClick ={() => setOn(!on)}>Show/Hide</button>
+        </div>
+               
+      )}/>
+      <ToggleRPC>
+          {({on, setOn, style}) => (
+            <div>
+                {on && <h1>I will not!</h1>}
+                <button style={style} onClick ={() => setOn(!on)}>Show/Hide</button>
+            </div>
+          )}
+      </ToggleRPC>
+
+
     </div>
   );
 }
